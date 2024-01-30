9b5635414d634b0f4a90f87338bd4529f8bebc4f
ferrazmanu <ferraz.manuela@hotmail.com>
2024-01-30 10:19:14 -0300

    eslint prettier and packages config

---
+++ .eslintrc.js
@@ -0,0 +1,66 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended',
+        'plugin:prettier/recommended',
+    ],
+    overrides: [
+        {
+            env: {
+                node: true,
+            },
+            files: ['.eslintrc.{js,cjs}'],
+            parserOptions: {
+                sourceType: 'script',
+            },
+        },
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: [
+        '@typescript-eslint',
+        'react',
+        'react-hooks',
+        'prettier',
+        'import',
+    ],
+    rules: {
+        semi: ['error', 'always'],
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        'react/jsx-one-expression-per-line': 'off',
+        'no-use-before-define': 'off',
+        'react/prop-types': 'off',
+        'react-hooks/exhaustive-deps': 'warn',
+        'react/jsx-filename-extension': [
+            1,
+            {
+                extensions: ['.tsx'],
+            },
+        ],
+        'import/prefer-default-export': 'off',
+        'import/extensions': [
+            'error',
+            'ignorePackages',
+            {
+                ts: 'never',
+                tsx: 'never',
+            },
+        ],
+    },
+    settings: {
+        'import/resolver': {
+            typescript: {},
+        },
+        react: {
+            version: 'detect',
+        },
+    },
+}
---
+++ src/styles/global.ts
@@ -0,0 +1,34 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+   *{
+       margin: 0;
+       padding: 0;
+       outline:0;
+       box-sizing:border-box;
+       font-family: 'Open Sans', sans-serif; 
+   }
+   #root{
+       margin:0 auto;
+   }
+   .App {
+  text-align: center;
+}
+
+  body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+      monospace;
+  }
+
+`;
+
+export default GlobalStyle;
